52675306ac598352f98eda733d9373dedc85c4b8
reinablqs <119019800+reinablqs@users.noreply.github.com>
2026-06-20 23:55:50 +0700

    Full update

--- dist/js/script.js
+++ dist/js/script.js
@@ -13,7 +13,9 @@ window.onscroll = function() {
 
 // Hamburger
 const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active')
+    navMenu.classList.toggle('hidden');
 });
